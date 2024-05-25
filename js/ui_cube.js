1245a8b27070b62240b853c89d84582a2afe3018
Er. Reda <redaermich@gmail.com>
2024-05-25 21:38:11 +0100

    feature: setup the project

---
+++ js/ui_cube.js
@@ -0,0 +1,21 @@
+// ui_cube.js
+// ==========
+// UI event handlers for cube
+//
+//  AUTHOR: Song Ho Ahn (song.ahn@gmail.com)
+// CREATED: 2024-05-07
+// UPDATED: 2024-05-10
+///////////////////////////////////////////////////////////////////////////////
+
+// Must use IIFE to avoid _let_ variable redeclaration errors
+(() =>
+{
+    let labelSide = document.getElementById("labelSide");
+    let rangeSide = document.getElementById("rangeSide");
+    rangeSide.value = 1;
+    rangeSide.addEventListener("input", e =>
+    {
+        labelSide.innerText = rangeSide.value;
+        gl.model.setSideLength(parseFloat(rangeSide.value));
+    });
+})();
